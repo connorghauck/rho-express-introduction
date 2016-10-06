9e36f076f2466b21fd636357fbf196e7931a9f4b
Connor Hauck <connorghauck@yahoo.com>
2016-10-05 21:50:07 -0500

    First commit

--- public/js/client.js
+++ public/js/client.js
@@ -1,34 +1,42 @@
 $(function(){
-  // ask the server for songs, and then draw them
-  getSongs();
+    //ask server for songs, then draw them
+    getSongs();
 
-  // listen for submit events and send new songs to the server
-  $('form').on('submit', function(event){
-    event.preventDefault();
-    var formData = $(this).serialize();
-    $.ajax({
-      type: 'POST',
-      url: '/songs',
-      data: formData,
-      success: getSongs
+
+    //listen for submit events and send new songs to the server
+    $('form').on('submit', function(event){
+        event.preventDefault();
+
+        var formData = $(this).serialize(); //serialize gives you the raw string as opposed to serializearray
+//then send that data off to the server
+        $.ajax({
+            type: 'POST',
+            url: '/songs',
+            data: formData,
+            success: getSongs,
+        });
+        $(this).find('input[type=text]').val('');
     });
-
-    $(this).find('input[type=text]').val('');
-  });
 });
 
-function getSongs() {
-  $.ajax({
+
+
+
+function getSongs(){
+$.ajax({
     type: 'GET',
     url: '/songs',
+    //we need to wait for the server to respond!
     success: function(songs){
-      $('#songs').empty();
-      songs.forEach(function(song){
-        var $li = $('<li></li>');
-        $li.append('<p>'+ song.title + '</p>');
-        $li.append('<p>by: '+ song.artist + '</p>');
-        $('#songs').append($li);
-      });
+        $('#songs').empty();
+        songs.forEach(function(song){
+            var $li = $('<li></li>');
+            $li.append('<p>' + song.title + '</p>');
+            $li.append('<p>by: ' + song.artist + '</p>');
+            $li.append('<p>' + song.date + '</p>');
+
+            $('#songs').append($li);
+        });
     }
-  });
+});
 }
--- server.js
+++ server.js
@@ -4,48 +4,81 @@ var path = require('path');
 
 var app = express();
 
+
+
+
 // middleware function, gets executed on each request
 app.use(function(req, res, next){
-  console.log('Got a request!');
-  next();
+    console.log('Got a request!');
+    next();
 });
+
 
 // middleware for parsing the body and turning it into a JS object
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+
+
 app.post('/', function(req, res){
-  console.log('req.body=', req.body);
-  res.sendStatus(200);
+    console.log('req.body:', req.body);
+    res.sendStatus(200);
 });
 
-app.get('/', function(req, res){
-  console.log('Received a request at', new Date());
-  // __dirname is the folder this file lives in
-  var filename = path.join(__dirname, 'public/views/index.html');
-  console.log('filename:', filename);
-  res.sendFile(filename);
-});
+
+
+
+app.get('/', function(req, res){ //route (GET, POST, ETC.) + path ('/' '/kittens') + handler ('function(){}')
+    console.log('Received a request at', new Date());
+
+
+    var filename = path.join(__dirname, 'public/views/index.html'); //__dirname is a global variable that represents the directory that this specific file lives in
+    console.log('filename:', filename);
+    res.sendFile(filename);
+
+//this basically takes the folder that index.html is in and then send index.html from that folder.
+
+//res.send('Hello world!'); //the response object is going to hav ea few methods associated with it, one of the most common being the send method.
+});//telling express to listen specifically for get requests. first you need the path '/', then you tell it what to do once it gets the request
+//this is a request and response set of parameters. the res is the response you are sending back to the client from the server.
 
 app.get('/kittens', function(req, res){
-  console.log('Query params:', req.query);
-  if (req.query.age > 2) {
+
+    console.log('Query params:', req.query); //this will give us an object containing all query parameters associated with this particular request.
+    if (req.query.age > 2) {
     res.send('MEOW!');
-  } else {
+    } else {
     res.send('meow');
-  }
+    }
 });
 
-var songs = [];
+var songs = []; //create a variable called songs which we will eventually fill with songs, making it an empty array
 
 app.post('/songs', function(req, res){
-  console.log('req.body:', req.body);
-  songs.push(req.body);
-  console.log('songs', songs);
-  res.sendStatus(200);
+
+    var NotTheSame = true; //make a variable to check if things are unique
+        songs.forEach(function(song){ //loop through each and check if they're unique
+            if (req.body.artist.trim() == song.artist.trim() && req.body.title.trim() == song.title.trim()){
+                NotTheSame = false;  //if they aren't, NotTheSame becomes false
+            }
+        });
+
+
+
+
+if (NotTheSame){
+    var date = new Date; //create a new Date
+    req.body.date = date.toDateString();//turn that into the date string as req.body so you can add it to the DOM
+    songs.push(req.body);  //if it's unique it will push the inputs to req.body and send the 200 status
+    res.sendStatus(200);
+} else {
+    res.sendStatus(404); //if not unique it will 404
+}
 });
 
+
 app.get('/songs', function(req, res){
-  res.send(songs);
+    res.send(songs);
 });
 
 // middleware for serving static files
